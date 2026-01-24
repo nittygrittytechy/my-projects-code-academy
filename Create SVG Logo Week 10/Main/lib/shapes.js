534960e309b72aac5fa77dd1f36c566f4ed8c1ce
Jennifer <nittygrittytechy@protonmail.com>
2026-01-24 11:20:37 -0600

    Adding SVG Logo Week 10 Challenge

---
+++ Create SVG Logo Week 10/Main/lib/shapes.js
@@ -0,0 +1,34 @@
+// Base class for shapes
+class Shape {
+  constructor() {
+    this.color = "";
+  }
+  setColor(color) {
+    this.color = color;
+  }
+}
+
+class Circle extends Shape {
+  render() {
+    return `<circle cx="150" cy="100" r="80" fill="${this.color}" />`;
+  }
+}
+
+class Triangle extends Shape {
+  render() {
+    return `<polygon points="150, 18 244, 182 56, 182" fill="${this.color}" />`;
+  }
+}
+
+class Square extends Shape {
+  render() {
+    return `<rect x="90" y="40" width="120" height="120" fill="${this.color}" />`;
+  }
+}
+
+class Rectangle extends Shape {
+  render() {
+    return `<rect x="50" y="50" width="200" height="100" fill="${this.color}" />`;
+  }
+}
+module.exports = { Circle, Triangle, Square, Rectangle };
---
+++ Create SVG Logo Week 10/Main/lib/shapes.test.js
@@ -0,0 +1,75 @@
+const { Square, Triangle, Circle, Rectangle } = require("./shapes");
+
+describe("Circle", () => {
+  test("should render svg for a green circle element", () => {
+    const expectedSvg = '<circle cx="150" cy="100" r="80" fill="green" />';
+    const circle = new Circle();
+    circle.setColor("green");
+    const actualSvg = circle.render();
+    expect(actualSvg).toEqual(expectedSvg);
+  });
+  test("should accept a fillColor param", () => {
+    const expectedSvg = '<circle cx="150" cy="100" r="80" fill="blue" />';
+    const circle = new Circle();
+    circle.setColor("blue");
+    const actualSvg = circle.render();
+    expect(actualSvg).toEqual(expectedSvg);
+  });
+});
+
+describe("Triangle", () => {
+  test("should render svg for a green triangle element", () => {
+    const expectedSvg =
+      '<polygon points="150, 18 244, 182 56, 182" fill="bisque" />';
+    const triangle = new Triangle();
+    triangle.setColor("bisque");
+    const actualSvg = triangle.render();
+    expect(actualSvg).toEqual(expectedSvg);
+  });
+  test("should accept a fillColor param", () => {
+    const expectedSvg =
+      '<polygon points="150, 18 244, 182 56, 182" fill="purple" />';
+    const triangle = new Triangle();
+    triangle.setColor("purple");
+    const actualSvg = triangle.render();
+    expect(actualSvg).toEqual(expectedSvg);
+  });
+});
+
+describe("Square", () => {
+  test("should render svg for a green polygon element", () => {
+    const expectedSvg =
+      '<rect x="90" y="40" width="120" height="120" fill="dodgerblue" />';
+    const square = new Square();
+    square.setColor("dodgerblue");
+    const actualSvg = square.render();
+    expect(actualSvg).toEqual(expectedSvg);
+  });
+  test("should accept a fillColor param", () => {
+    const expectedSvg =
+      '<rect x="90" y="40" width="120" height="120" fill="red" />';
+    const square = new Square();
+    square.setColor("red");
+    const actualSvg = square.render();
+    expect(actualSvg).toEqual(expectedSvg);
+  });
+});
+
+describe("Rectangle", () => {
+  test("should render svg for a pink rectangle element", () => {
+    const expectedSvg =
+      '<rect x="50" y="50" width="200" height="100" fill="pink" />';
+    const rectangle = new Rectangle();
+    rectangle.setColor("pink");
+    const actualSvg = rectangle.render();
+    expect(actualSvg).toEqual(expectedSvg);
+  });
+  test("should accept a fillColor param", () => {
+    const expectedSvg =
+      '<rect x="50" y="50" width="200" height="100" fill="red" />';
+    const rectangle = new Rectangle();
+    rectangle.setColor("red");
+    const actualSvg = rectangle.render();
+    expect(actualSvg).toEqual(expectedSvg);
+  });
+});
